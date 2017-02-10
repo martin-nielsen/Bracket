f6090dfb3c073e7c4db27faea0258afae1bb5eb3
Martin Nielsen <Martin Nielsen>
2017-02-11 00:07:44 +0100

    Decided to go with SQL Express instead

--- Brackets/Client/js/app.js
+++ Brackets/Client/js/app.js
@@ -1,48 +1,11 @@
-// create our angular module and inject firebase
-angular.module('bracketsApp', ['firebase'])
+// create our angular module
+angular.module('bracketsApp', [''])
 
 // create our main controller and get access to firebase
-.controller('mainController', function($scope, $firebase) {
-	
-	//Connect to firebase
-	var ref = new Firebase("https://testproject-48f57.firebaseio.com/days");
-	var fb = $firebase(ref);
-
-	//sync as object
-	var syncObject = fb.$asObject();
-	//Three way data binding
-	syncObject.$bindTo($scope, 'days');
+.controller('mainController', function($scope) {
 
 	$scope.reset = function(){
-		fb.$set({
-			monday: {
-				name: 'Monday',
-				slots: {
-					0900: {
-						time: '9:00',
-						booked: false
-					},
-					1100: {
-						time: '11:00',
-						booked: false
-					}
-
-				}
-			},
-			tuesday: {
-				name: 'Tuesday',
-				slots: {
-					0900: {
-						time: '09:00',
-						booked: false
-					},
-					1100: {
-						time: '09:00',
-						booked: false
-					}
-				}
-			}
-		});
+		
 	};
 
 	// our application code will go here
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bracket
 Just testing some stuff with git.
 More testing.
+Will use SQL server express
+angular for spa
