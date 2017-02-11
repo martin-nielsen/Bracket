d9ab1066e43cb90d1cbfa8641da9674edabdf27a
Martin Nielsen <Martin Nielsen>
2017-02-11 19:44:57 +0100

    commit, added some serverside NodeJS

--- Brackets/Client/js/app.js
+++ Brackets/Client/js/app.js
@@ -1,12 +1,17 @@
+// A module is a collection of services, directives, 
+// controllers, filters, and configuration information. 
+// angular.module is used to configure the $injector.
+
 // create our angular module
-angular.module('bracketsApp', [''])
-
-// create our main controller and get access to firebase
+var bracketModule = angular.module('bracketsModule', [''])
+// code to register a new service
+bracketModule.value('appName', 'bracketsApp')
+// create our main controller
 .controller('mainController', function($scope) {
-
+	// our application code will go here
+	// Function that runs when reset button is clicked.
 	$scope.reset = function(){
-		
+	
 	};
 
-	// our application code will go here
 });
---
+++ Brackets/Server/nodeServer.js
@@ -0,0 +1,15 @@
+// Server code see:https://nodejs.org/dist/latest-v6.x/docs/api/synopsis.html for examples
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://', $hostname,':',$port,'/');
+});
